5eb731bee003013b22ec2148a7ad3fb0a5455820
sudzekai <yomakum@mail.ru>
2024-11-25 03:02:09 +0300

    Изменен дизайн окна, изменен основной метод добавления процессов для завершения: теперь в списке процессов отображаются все процессы, запущенные на данный момент, из которых можно выбрать те, что нужно завершать, присутствует возможность поиска процесса, упразднена возможность вписывать названия процессов для завершения

--- KillProcess/MainWindow.xaml.cs
+++ KillProcess/MainWindow.xaml.cs
@@ -1,28 +1,30 @@
 ﻿using System.Diagnostics;
 using System.IO;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Timers;
-using System.Reflection.Emit;
-using System.Threading;
+using System.Xml.Linq;
 
 
 namespace KillProcess
 {
     public partial class MainWindow : Window
     {
-
+        string path = "proc.txt";
         private bool ongoing = false;
         public MainWindow()
         {
-
-            
             try
             {
+                if (!File.Exists(path))
+                {
+                    File.WriteAllText(path, "");
+                }
                 InitializeComponent();
                 this.Show();
-                ProcListAppender();
+                PROCESSESListAdder();
+
             }
             catch (Exception ex)
             {
@@ -31,59 +33,89 @@ namespace KillProcess
 
         }
 
+        private void PROCESSESListAdder()
+        {
+            for (int i = 0; i < Process.GetProcesses().Count(); i++)
+            {
+                Process proc = Process.GetProcesses()[i];
+                CheckBox box = new CheckBox();
+                foreach (string name in File.ReadAllLines(path))
+                {
+                    if (name == proc.ProcessName)
+                    {
+                        box.IsChecked = true;
+                    }
+                }
+                box.FontFamily = new FontFamily("Comic Sans MS");
+                box.FontSize = 15;
+                box.Foreground = Brushes.White;
+                box.Content = proc.ProcessName;
+                box.Checked += Check;
+                box.Unchecked += UnCheck;
+
+
+                PROCESSESList.Items.Add(box);
+
+            }
+
+        }
+
+
+
+
         private void KillProcess()
         {
             DateTime starttime = DateTime.Now;
             async Task LoopAsync()
             {
-                
+
                 while (ongoing)
-                {
-                    CounterLabel.Content = new DateTime((DateTime.Now - starttime).Ticks).ToString("mm:ss:ff");
-                    foreach (string processName in ProcessList.Items)
+                { 
+                    foreach (string name in File.ReadAllLines(path))
                     {
-                        Process[] proc = Process.GetProcessesByName(processName);
+                        
+                        //CounterLabel.Content = new DateTime((DateTime.Now - starttime).Ticks).ToString("mm:ss");
+                        Process[] proc = Process.GetProcessesByName(name.Trim());
                         foreach (var process in proc)
                         {
-                            try
+                            if (name.Length == 0)
                             {
-                                process.Kill();
+                                continue;
                             }
-                            catch (Exception ex)
+                            else
                             {
-                                var converter = new BrushConverter();
-                                ongoing = false;
-                                Start.Content = "Старт";
-                                Start.BorderBrush = (Brush)converter.ConvertFrom("#7160e8");
-                                if (processName.Length == 0)
+                                try
                                 {
-                                    MessageBox.Show("Удалите пустую строку из списка процессов, либо перезапустите приложение", "Ошибка", MessageBoxButton.OK);
+                                    process.Kill();
                                 }
-                                else
+                                catch (Exception ex)
                                 {
-                                    MessageBox.Show($"Процесс - {processName}\n{ex}", "Ошибка", MessageBoxButton.OK);
+                                    var converter = new BrushConverter();
+                                    ongoing = false;
+                                    Start.Content = "Старт";
+                                    Start.BorderBrush = (Brush)converter.ConvertFrom("#7160e8");
                                 }
 
-                                break;
                             }
                         }
+                        await Task.Delay(1000);
                     }
-                    await Task.Delay(1000);
                 }
+                
             }
+
             _ = LoopAsync().ContinueWith(t =>
             {
             });
-
         }
 
         private void Start_Click(object sender, RoutedEventArgs e)
         {
-            
+
             var converter = new BrushConverter();
             if (!ongoing)
             {
-                CounterLabel.Visibility = Visibility.Visible;
+                //CounterLabel.Visibility = Visibility.Visible;
                 ongoing = true;
                 Start.Content = " Вы в\nпотоке";
                 Start.FontSize = 25;
@@ -92,7 +124,7 @@ namespace KillProcess
             }
             else
             {
-                CounterLabel.Visibility = Visibility.Hidden;
+                //CounterLabel.Visibility = Visibility.Hidden;
                 ongoing = false;
                 Start.Content = "Старт";
                 Start.FontSize = 25;
@@ -101,69 +133,96 @@ namespace KillProcess
 
         }
 
-        private void ProcessList_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Delete)
-            {
-                ProcessList.Items.Remove(ProcessList.SelectedItem);
-                ProcListToFile();
-            }
-        }
-
-        private void InputProcessName_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                ProcessList.Items.Add(InputProcessName.Text);
-                InputProcessName.Text = null;
-                ProcListToFile();
-            }
-
-        }
-
-        private void OptionsBtn_Click(object sender, RoutedEventArgs e)
-        {
-            Options.Visibility = Visibility.Hidden;
-        }
-
         private void OpenOptions_Click(object sender, RoutedEventArgs e)
         {
-            Options.Visibility = Visibility.Visible;
-        }
-
-        private void ProcListAppender()
-        {
-            string path = "ProcessList.txt";
-            if (!File.Exists(path))
+            if (OPTIONSBar.Visibility == Visibility.Visible)
             {
-                File.WriteAllText(path, "");
+                OPTIONSBar.Visibility = Visibility.Hidden;
+                AdditionalTOOLBar.Visibility = Visibility.Hidden;
+                FINDWindow.Visibility = Visibility.Hidden;
+
             }
             else
             {
-                if (File.ReadAllText(path).Trim().Length > 0)
-                {
-                    for (int i = 0; i < File.ReadAllText(path).Split(' ').Count(); i++)
-                    {
-                        ProcessList.Items.Add(File.ReadAllText(path).Split()[i].Trim());
-
-                    }
-                }
+                OPTIONSBar.Visibility = Visibility.Visible;
+                AdditionalTOOLBar.Visibility = Visibility.Visible;
+                FINDWindow.Visibility = Visibility.Hidden;
             }
+        }
 
+        private void OpenBp_Click(object sender, RoutedEventArgs e)
+        {
 
         }
 
-        private void ProcListToFile()
+        private void OpenInfo_Click(object sender, RoutedEventArgs e)
         {
-            string path = "ProcessList.txt";
-            File.WriteAllText(path, "");
-            string text = "";
-            foreach (string item in ProcessList.Items)
-            {
-                text += item + " ";
 
+        }
+
+        private void FINDBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (FINDWindow.Visibility == Visibility.Visible)
+            {
+                FINDWindow.Visibility = Visibility.Hidden;
+                FINDTextBox.Text = string.Empty;
             }
-            File.AppendAllText(path, $"{text.TrimEnd()}");
+            else
+            {
+                FINDWindow.Visibility = Visibility.Visible;
+                FINDTextBox.Text = string.Empty;
+            }
+        }
+
+        private void FINDTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                FindProc(FINDTextBox.Text);
+            }
+        }
+
+        private void FindProc(string text)
+        {
+            PROCESSESList.Items.Clear();
+
+            foreach (Process proc in Process.GetProcesses())
+            {
+                if (proc.ProcessName.ToLower().StartsWith(text.ToLower()))
+                {
+                    CheckBox box = new CheckBox();
+                    foreach (string name in File.ReadAllLines(path))
+                    {
+                        if (name == proc.ProcessName)
+                        {
+                            box.IsChecked = true;
+                        }
+                    }
+                    box.FontFamily = new FontFamily("Comic Sans MS");
+                    box.FontSize = 15;
+                    box.Foreground = Brushes.White;
+                    box.Content = proc.ProcessName;
+                    box.Checked += Check;
+                    box.Unchecked += UnCheck;
+
+                    PROCESSESList.Items.Add(box);
+                }
+            }
+        }
+
+        private void Check(object sender, RoutedEventArgs e)
+        {
+            CheckBox chBox = (CheckBox)sender;
+            File.AppendAllText(path, $"{chBox.Content.ToString()}\n");
+
+        }
+        private void UnCheck(object sender, RoutedEventArgs e)
+        {
+            CheckBox chBox = (CheckBox)sender;
+
+            string text = File.ReadAllText(path);
+            text = text.Replace(chBox.Content.ToString(), null);
+            File.WriteAllText(path, text);
 
         }
     }
